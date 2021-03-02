c783ddaee7ca4c06aa35130e42df428c2ab31512
YoungJin Lim <yjlim@clamos.io>
2021-03-02 18:47:36 +0900

    update.

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(FriendlyChatApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class FriendlyChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // primarySwatch: Colors.blue,
-        // visualDensity: VisualDensity.adaptivePlatformDensity,
-        primaryColor: Colors.white
-      ),
-      home: RandomWords()
+      title: 'Friendly Chat',
+      home: ChatScreen()
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class ChatScreen extends StatefulWidget {
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _ChatScreenState createState() => _ChatScreenState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = Set<WordPair>();
-  final _biggerFont = TextStyle(fontSize: 18.0);
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
 
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      }
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final tiles = _saved.map((WordPair pair) {
-            return ListTile(
-              title: Text(
-                pair.asPascalCase,
-                style: _biggerFont
-              )
-            );
-          });
-
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestios')
-            ),
-            body: ListView(children: divided)
-          );
-        }
+  Widget _buildTextComposer() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(
+          hintText: 'Send a message'
+        )
       )
     );
+  }
+
+  void _handleSubmitted(String value) {
+    _textController.clear();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.list),
-            onPressed: _pushSaved
-          )
-        ]
+        title: Text('Friendly Chat')
       ),
-      body: _buildSuggestions()
+      body: _buildTextComposer()
     );
   }
 }
